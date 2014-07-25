291d8e3186eb880aed1a7f4d42fd5a43e00bbca7
hever <hpumallihua@gmail.com>
2014-07-25 01:19:32 -0500

    sd

---
+++ appWebCineplanet/src/main/java/pe/org/cineplanet/dto/ReporteDTO.java
@@ -0,0 +1,229 @@
+package pe.org.cineplanet.dto;
+
+import java.math.BigDecimal;
+
+public class ReporteDTO {
+
+	private Long idTipoEntrada;
+	private BigDecimal precio;
+	private Integer cantidad;
+
+	private String fechaPedido;
+	private String idAgencia;
+	private String nombres;
+	private Integer cantEntrada17;
+	private Integer cantEntrada10;
+	private Integer cantEntrada9;
+	private Integer cantEntrada7;
+
+	private Integer cantCombo11;
+	private Integer cantCombo10;
+	private Integer cantCombo9;
+	private Integer cantCombo7;
+
+	private BigDecimal totalCliente;
+	private BigDecimal totalVenta;
+
+	private Integer totalEntrada17;
+	private Integer totalEntrada10;
+	private Integer totalEntrada9;
+	private Integer totalEntrada7;
+
+	private Integer totalCombo11;
+	private Integer totalCombo10;
+	private Integer totalCombo9;
+	private Integer totalCombo7;
+
+	public String getFechaPedido() {
+		return fechaPedido;
+	}
+
+	public void setFechaPedido(String fechaPedido) {
+		this.fechaPedido = fechaPedido;
+	}
+
+	public String getIdAgencia() {
+		return idAgencia;
+	}
+
+	public void setIdAgencia(String idAgencia) {
+		this.idAgencia = idAgencia;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public Integer getCantEntrada17() {
+		return cantEntrada17;
+	}
+
+	public void setCantEntrada17(Integer cantEntrada17) {
+		this.cantEntrada17 = cantEntrada17;
+	}
+
+	public Integer getCantEntrada10() {
+		return cantEntrada10;
+	}
+
+	public void setCantEntrada10(Integer cantEntrada10) {
+		this.cantEntrada10 = cantEntrada10;
+	}
+
+	public Integer getCantEntrada9() {
+		return cantEntrada9;
+	}
+
+	public void setCantEntrada9(Integer cantEntrada9) {
+		this.cantEntrada9 = cantEntrada9;
+	}
+
+	public Integer getCantEntrada7() {
+		return cantEntrada7;
+	}
+
+	public void setCantEntrada7(Integer cantEntrada7) {
+		this.cantEntrada7 = cantEntrada7;
+	}
+
+	public Integer getCantCombo11() {
+		return cantCombo11;
+	}
+
+	public void setCantCombo11(Integer cantCombo11) {
+		this.cantCombo11 = cantCombo11;
+	}
+
+	public Integer getCantCombo10() {
+		return cantCombo10;
+	}
+
+	public void setCantCombo10(Integer cantCombo10) {
+		this.cantCombo10 = cantCombo10;
+	}
+
+	public Integer getCantCombo9() {
+		return cantCombo9;
+	}
+
+	public void setCantCombo9(Integer cantCombo9) {
+		this.cantCombo9 = cantCombo9;
+	}
+
+	public Integer getCantCombo7() {
+		return cantCombo7;
+	}
+
+	public void setCantCombo7(Integer cantCombo7) {
+		this.cantCombo7 = cantCombo7;
+	}
+
+	public BigDecimal getTotalCliente() {
+		return totalCliente;
+	}
+
+	public void setTotalCliente(BigDecimal totalCliente) {
+		this.totalCliente = totalCliente;
+	}
+
+	public BigDecimal getTotalVenta() {
+		return totalVenta;
+	}
+
+	public void setTotalVenta(BigDecimal totalVenta) {
+		this.totalVenta = totalVenta;
+	}
+
+	public Integer getTotalEntrada17() {
+		return totalEntrada17;
+	}
+
+	public void setTotalEntrada17(Integer totalEntrada17) {
+		this.totalEntrada17 = totalEntrada17;
+	}
+
+	public Integer getTotalEntrada10() {
+		return totalEntrada10;
+	}
+
+	public void setTotalEntrada10(Integer totalEntrada10) {
+		this.totalEntrada10 = totalEntrada10;
+	}
+
+	public Integer getTotalEntrada9() {
+		return totalEntrada9;
+	}
+
+	public void setTotalEntrada9(Integer totalEntrada9) {
+		this.totalEntrada9 = totalEntrada9;
+	}
+
+	public Integer getTotalEntrada7() {
+		return totalEntrada7;
+	}
+
+	public void setTotalEntrada7(Integer totalEntrada7) {
+		this.totalEntrada7 = totalEntrada7;
+	}
+
+	public Integer getTotalCombo11() {
+		return totalCombo11;
+	}
+
+	public void setTotalCombo11(Integer totalCombo11) {
+		this.totalCombo11 = totalCombo11;
+	}
+
+	public Integer getTotalCombo10() {
+		return totalCombo10;
+	}
+
+	public void setTotalCombo10(Integer totalCombo10) {
+		this.totalCombo10 = totalCombo10;
+	}
+
+	public Integer getTotalCombo9() {
+		return totalCombo9;
+	}
+
+	public void setTotalCombo9(Integer totalCombo9) {
+		this.totalCombo9 = totalCombo9;
+	}
+
+	public Integer getTotalCombo7() {
+		return totalCombo7;
+	}
+
+	public void setTotalCombo7(Integer totalCombo7) {
+		this.totalCombo7 = totalCombo7;
+	}
+
+	public Long getIdTipoEntrada() {
+		return idTipoEntrada;
+	}
+
+	public void setIdTipoEntrada(Long idTipoEntrada) {
+		this.idTipoEntrada = idTipoEntrada;
+	}
+
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+}
